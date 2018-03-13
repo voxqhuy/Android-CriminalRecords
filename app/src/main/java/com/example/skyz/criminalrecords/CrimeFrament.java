6161311744e18683a3e7f3c20911bc0811e75d75
voxqhuy <kohnewlife@gmail.com>
2018-03-13 00:43:11 -0400

    added crime fragment

---
+++ app/src/main/java/com/example/skyz/criminalrecords/CrimeFrament.java
@@ -0,0 +1,29 @@
+package com.example.skyz.criminalrecords;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Skyz on 3/13/2018.
+ */
+
+public class CrimeFrament extends Fragment {
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        return v;
+    }
+}
