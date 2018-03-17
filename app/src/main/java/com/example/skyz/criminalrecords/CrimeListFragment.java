c583d21d50a2ce36bd7dd84f2aa5738fd681d2d6
voxqhuy <kohnewlife@gmail.com>
2018-03-17 11:56:23 -0400

    added CrimeListFragment and update Manifest

---
+++ app/src/main/java/com/example/skyz/criminalrecords/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package com.example.skyz.criminalrecords;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Skyz on 3/17/2018.
+ */
+
+public class CrimeListFragment extends Fragment{
+}
