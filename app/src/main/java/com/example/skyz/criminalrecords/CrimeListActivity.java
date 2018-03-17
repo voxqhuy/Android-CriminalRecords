e9d044e798049a66f87c7a428071a16ba0b8017b
voxqhuy <kohnewlife@gmail.com>
2018-03-17 11:53:12 -0400

    added CrimeListActivity

---
+++ app/src/main/java/com/example/skyz/criminalrecords/CrimeListActivity.java
@@ -0,0 +1,14 @@
+package com.example.skyz.criminalrecords;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Skyz on 3/17/2018.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return CrimeListFragment();
+    }
+}
