3fc66481c081a4cefa07d6372b5cd154aefd0859
voxqhuy <kohnewlife@gmail.com>
2018-03-17 15:16:04 -0400

    added recyclerview for  crime list fragment

--- app/src/main/java/com/example/skyz/criminalrecords/CrimeListFragment.java
+++ app/src/main/java/com/example/skyz/criminalrecords/CrimeListFragment.java
@@ -1,10 +1,29 @@
 package com.example.skyz.criminalrecords;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by Skyz on 3/17/2018.
  */
 
 public class CrimeListFragment extends Fragment{
+    private RecyclerView mCrimeRecyclerView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        mCrimeRecyclerView = view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
 }
