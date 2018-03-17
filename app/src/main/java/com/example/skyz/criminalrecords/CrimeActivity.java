9ded6169e047369dd624404560e3d9df6724b281
voxqhuy <kohnewlife@gmail.com>
2018-03-17 11:43:24 -0400

    extend the new abstract class in crime activity

--- app/src/main/java/com/example/skyz/criminalrecords/CrimeActivity.java
+++ app/src/main/java/com/example/skyz/criminalrecords/CrimeActivity.java
@@ -1,21 +1,11 @@
 package com.example.skyz.criminalrecords;
 
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class CrimeActivity extends AppCompatActivity {
+public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_fragment);
-
-        android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
-        if (fragment == null) {
-            fragment = new CrimeFragment();
-            fm.beginTransaction().add(R.id.fragment_container, fragment).commit();
-        }
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
