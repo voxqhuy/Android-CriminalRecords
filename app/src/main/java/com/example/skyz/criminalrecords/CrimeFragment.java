2a4d48993124265505986d3ec0bf70bf0ee3b2aa
voxqhuy <kohnewlife@gmail.com>
2018-04-13 12:54:20 -0400

    send data from activity to hosted fragment through bundles + retrieve data in the fragment

--- app/src/main/java/com/example/skyz/criminalrecords/CrimeFragment.java
+++ app/src/main/java/com/example/skyz/criminalrecords/CrimeFragment.java
@@ -40,8 +40,7 @@ public class CrimeFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        UUID crimeId = (UUID) getActivity().getIntent()
-                .getSerializableExtra(CrimeActivity.EXTRA_CRIME_ID);
+        UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
         mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
     }
 
