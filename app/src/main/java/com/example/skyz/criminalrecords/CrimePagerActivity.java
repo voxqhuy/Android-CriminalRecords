a2f4138d3a33d90eefdb9ad5c4680fa511257276
voxqhuy <kohnewlife@gmail.com>
2018-04-14 13:06:38 -0400

    added "jump to first", "jump to last" buttions

--- app/src/main/java/com/example/skyz/criminalrecords/CrimePagerActivity.java
+++ app/src/main/java/com/example/skyz/criminalrecords/CrimePagerActivity.java
@@ -9,6 +9,8 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,6 +20,8 @@ public class CrimePagerActivity extends AppCompatActivity {
     private static final String EXTRA_CRIME_ID = "com.example.skyz.criminalrecords.crime_id";
 
     private ViewPager mViewPager;
+    private Button mMoveToFirstBtn;
+    private Button mMoveToLastBtn;
     private List<Crime> mCrimes;
 
     public static Intent newIntent(Context packageContext, UUID crimeId) {
@@ -33,7 +37,10 @@ public class CrimePagerActivity extends AppCompatActivity {
 
         UUID crimeId = (UUID) getIntent().getSerializableExtra(EXTRA_CRIME_ID);
 
+        // The views
         mViewPager = findViewById(R.id.crime_view_pager);
+        mMoveToFirstBtn = findViewById(R.id.move_to_first_btn);
+        mMoveToLastBtn = findViewById(R.id.move_to_last_btn);
 
         CrimeLab crimeLab = CrimeLab.get(this);
         mCrimes = crimeLab.getCrimes();
@@ -50,7 +57,14 @@ public class CrimePagerActivity extends AppCompatActivity {
                 return mCrimes.size();
             }
         });
-
         mViewPager.setCurrentItem(mCrimes.indexOf(crimeLab.getCrime(crimeId)));
+
+        mMoveToFirstBtn.setOnClickListener((View v) -> {
+                mViewPager.setCurrentItem(0);
+        });
+
+        mMoveToLastBtn.setOnClickListener((View v) -> {
+            mViewPager.setCurrentItem(mCrimes.size() - 1);
+        });
     }
 }
