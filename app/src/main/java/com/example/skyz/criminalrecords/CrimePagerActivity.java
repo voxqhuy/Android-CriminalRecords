843918212495f3dc5b4392d8b94414e3bfb84636
voxqhuy <kohnewlife@gmail.com>
2018-04-14 10:17:31 -0400

    added view pager activity and view

---
+++ app/src/main/java/com/example/skyz/criminalrecords/CrimePagerActivity.java
@@ -0,0 +1,17 @@
+package com.example.skyz.criminalrecords;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+public class CrimePagerActivity extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+    }
+}
