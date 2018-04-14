57785cf5d7f13d7e3e303180f7c5b8860254bc38
voxqhuy <kohnewlife@gmail.com>
2018-04-14 00:01:38 -0400

    change CrimeLab to store LinkedHashMap of crimes (for constant search => improve performance)

--- app/src/main/java/com/example/skyz/criminalrecords/CrimeLab.java
+++ app/src/main/java/com/example/skyz/criminalrecords/CrimeLab.java
@@ -3,7 +3,9 @@ package com.example.skyz.criminalrecords;
 import android.content.Context;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -13,7 +15,7 @@ import java.util.UUID;
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
-    private List<Crime> mCrimes;
+    private Map<UUID, Crime> mCrimes;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -23,26 +25,21 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
-        mCrimes = new ArrayList<>();
+        mCrimes = new LinkedHashMap<>();
         for (int i = 0; i < 100; i++) {
             Crime crime = new Crime();
             crime.setTitle("Crime #" + i);
             crime.setSolved(i % 2 == 0);    // Every other one
             crime.setRequiresPolice(i % 2 == 0);
-            mCrimes.add(crime);
+            mCrimes.put(crime.getId(), crime);
         }
     }
 
     public List<Crime> getCrimes() {
-        return mCrimes;
+        return new ArrayList<>(mCrimes.values());   // return an array list of crimes
     }
 
     public Crime getCrime(UUID id) {
-        for (Crime crime : mCrimes) {
-            if (crime.getId().equals(id)) {
-                return crime;
-            }
-        }
-        return null;
+        return mCrimes.get(id);
     }
 }
