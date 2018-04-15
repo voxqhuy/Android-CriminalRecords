b8b4e614bb5274a88740dcda5bffff4ba338432b
voxqhuy <kohnewlife@gmail.com>
2018-04-15 15:38:16 -0400

    created a time picker

---
+++ app/src/main/java/com/example/skyz/criminalrecords/TimePickerFragment.java
@@ -0,0 +1,28 @@
+package com.example.skyz.criminalrecords;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+
+/**
+ * Created by Vo Huy on 4/15/2018.
+ */
+public class TimePickerFragment extends DialogFragment {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_time, null);
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(view)
+                .setTitle(R.string.time_picker_title)
+                .setPositiveButton(android.R.string.ok, null)
+                .create();
+
+    }
+}
